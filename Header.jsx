09c32028a284b5c75a0fa290929e516404e48e05
Aakarsh Sharma <aakarshjai@gmail.com>
2024-11-10 15:41:01 +0530

    header file

---
+++ Header.jsx
@@ -0,0 +1,16 @@
+
+function Header(){
+
+    return (
+        <header>
+            <h1>My website</h1>
+            <ul>
+                <li>apple</li>
+                <li>banana</li>
+                <li>orange</li>
+            </ul>
+        </header>
+    );
+}
+
+export default Header
